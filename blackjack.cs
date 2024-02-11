741c583a2793d8be86514f2e62671724f9806cb2
Simon <simonshapiro28@gmail.com>
2024-02-10 23:52:50 -0600

    add playGame prompt and string  to blackjack.cs

--- blackjack.cs
+++ blackjack.cs
@@ -6,6 +6,9 @@ int fourthCard = 0;
 int fifthCard = 0;
 int playerCardTotal = 0;
 
+//stores the user's response to the prompt "play Blackjack?"
+string? playGame = "";
+
 //opponent's card draws and the total value of all the opponent's cards
 int opponentCard1 = 0;
 int opponentCard2 = 0;
@@ -13,5 +16,7 @@ int opponentCard3 = 0;
 int opponentCardTotal = 0;
 
 Console.WriteLine("Would you like to play blackjack?");
+Console.WriteLine("Type 'yes' or 'no'.");
+playGame = Console.ReadLine();
 
 
