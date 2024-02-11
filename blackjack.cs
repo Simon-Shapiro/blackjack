ae6a79a7059aa809f2a1fd6fa2a32bb255499d51
Simon <simonshapiro28@gmail.com>
2024-02-10 23:24:49 -0600

    add ints for player and opponent card totals

--- blackjack.cs
+++ blackjack.cs
@@ -4,7 +4,9 @@ int thirdCard = new int;
 int fourthCard = new int;
 int fifthCard = new int;
 
+int playerCardTotal = new int;
 
 int opponentCard1 = new int;
 int opponentCard2 = new int;
 int opponentCard3 = new int;
+int opponentCardTotal = new int;
