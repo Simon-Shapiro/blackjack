da01060a16eb66405cdf29950cce91836438d098
Simon <simonshapiro28@gmail.com>
2024-02-11 15:57:23 -0600

    adding blackjack rules to game loop

--- blackjack.cs
+++ blackjack.cs
@@ -1,3 +1,6 @@
+Random number = new Random();
+
+
 //player's card draws and the total value of all the player's cards
 int firstCard = 0;
 int secondCard = 0;
@@ -29,6 +32,17 @@ playGame = Console.ReadLine();
 //Game Loop
 do
 {
+
+Console.WriteLine("BlackJack Rules:");
+Console.WriteLine("Aces are worth 1, Jack = 11, Queen = 12, King = 13. There are no jokers.");
+
+Console.WriteLine("You will begin with one card. At the start of the round, you will see the sum of your opponent's hand.");
+Console.WriteLine("Your goal is to get the sum off all cards in your hand as close as possible to 21, without going over.");
+Console.WriteLine("Continue drawing cards until the sum of your hand is greater than your opponent, but less than 21.");
+Console.WriteLine("If the sum of your cards exceeds 21, you lose. If your opponent's hand is closer to 21 than yours, you lose.");
+Console.WriteLine("If you beat your opponent's hand without going over 21, you win!");
+
+firstCard = 
 
 } while (playGame == "yes");
 
