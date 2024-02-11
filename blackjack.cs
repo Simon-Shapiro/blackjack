ca0699c2996a0c1dc2ff05ef9df9812f00f171ad
Simon <simonshapiro28@gmail.com>
2024-02-10 23:28:00 -0600

    add comments to outline player's & opponent's hands

--- blackjack.cs
+++ blackjack.cs
@@ -1,12 +1,17 @@
+//player's card draws and the total value of all the player's cards
 int firstCard = new int;
 int secondCard = new int;
 int thirdCard = new int;
 int fourthCard = new int;
 int fifthCard = new int;
-
 int playerCardTotal = new int;
 
+//opponent's card draws and the total value of all the opponent's cards
 int opponentCard1 = new int;
 int opponentCard2 = new int;
 int opponentCard3 = new int;
 int opponentCardTotal = new int;
+
+Console.WriteLine("Would you like to play blackjack?");
+
+
