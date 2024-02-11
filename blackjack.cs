03983abbae539403294190e9d32565f80b7a54e9
Simon <simonshapiro28@gmail.com>
2024-02-10 23:47:33 -0600

    repairing blackjack.csproj file

--- blackjack.cs
+++ blackjack.cs
@@ -1,16 +1,16 @@
 //player's card draws and the total value of all the player's cards
-int firstCard = new int;
-int secondCard = new int;
-int thirdCard = new int;
-int fourthCard = new int;
-int fifthCard = new int;
-int playerCardTotal = new int;
+int firstCard = 0;
+int secondCard = 0;
+int thirdCard = 0;
+int fourthCard = 0;
+int fifthCard = 0;
+int playerCardTotal = 0;
 
 //opponent's card draws and the total value of all the opponent's cards
-int opponentCard1 = new int;
-int opponentCard2 = new int;
-int opponentCard3 = new int;
-int opponentCardTotal = new int;
+int opponentCard1 = 0;
+int opponentCard2 = 0;
+int opponentCard3 = 0;
+int opponentCardTotal = 0;
 
 Console.WriteLine("Would you like to play blackjack?");
 
